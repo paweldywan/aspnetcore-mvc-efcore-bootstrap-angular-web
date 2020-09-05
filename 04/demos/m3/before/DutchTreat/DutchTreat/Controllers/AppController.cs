6999be55737b2675710c415b1ac72b25ffac223c
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-09-05 19:46:30 +0200

    Development

--- 04/demos/m3/before/DutchTreat/DutchTreat/Controllers/AppController.cs
+++ 04/demos/m3/before/DutchTreat/DutchTreat/Controllers/AppController.cs
@@ -1,4 +1,5 @@
-﻿using DutchTreat.ViewModels;
+﻿using DutchTreat.Services;
+using DutchTreat.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,13 @@ namespace DutchTreat.Controllers
 {
     public class AppController : Controller
     {
+        private readonly IMailService mailService;
+
+        public AppController(IMailService mailService)
+        {
+            this.mailService = mailService;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -23,6 +31,14 @@ namespace DutchTreat.Controllers
         [HttpPost("contact")]
         public IActionResult Contact(ContactViewModel model)
         {
+            if (ModelState.IsValid)
+            {
+                //Send the email
+                mailService.SendMessage("shawn@wildermuth.com", model.Subject, $"From: {model.Name} - {model.Email}, Message: {model.Message}");
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
+            }
+
             return View();
         }
 
