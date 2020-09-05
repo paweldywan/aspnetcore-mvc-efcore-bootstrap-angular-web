54a1713003ddcc4ff7fefea5d40cc81122485672
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-09-05 11:00:36 +0200

    Development

---
+++ 04/demos/m3/before/DutchTreat/DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,36 @@
+﻿using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+
+            return View();
+        }
+    }
+}
