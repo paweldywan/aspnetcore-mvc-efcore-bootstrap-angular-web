a0a32af983c8bcac640ed9b880b949fa4f169ec2
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-09-09 13:19:07 +0200

    Development

--- 11/demos/m10/before/DutchTreat/DutchTreat/Controllers/AppController.cs
+++ 11/demos/m10/before/DutchTreat/DutchTreat/Controllers/AppController.cs
@@ -11,56 +11,53 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DutchTreat.Controllers
 {
-  public class AppController : Controller
-  {
-    private readonly IMailService _mailService;
-    private readonly IDutchRepository _repository;
-
-    public AppController(IMailService mailService, IDutchRepository repository)
+    public class AppController : Controller
     {
-      _mailService = mailService;
-      _repository = repository;
+        private readonly IMailService _mailService;
+        private readonly IDutchRepository _repository;
+
+        public AppController(IMailService mailService, IDutchRepository repository)
+        {
+            _mailService = mailService;
+            _repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Send the email
+                _mailService.SendMessage("shawn@wildermuth.com", model.Subject, $"From: {model.Name} - {model.Email}, Message: {model.Message}");
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
+            }
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Shop()
+        {
+            return View();
+        }
     }
-
-    public IActionResult Index()
-    {
-      return View();
-    }
-
-    [HttpGet("contact")]
-    public IActionResult Contact()
-    {
-      return View();
-    }
-
-    [HttpPost("contact")]
-    public IActionResult Contact(ContactViewModel model)
-    {
-      if (ModelState.IsValid)
-      {
-        // Send the email
-        _mailService.SendMessage("shawn@wildermuth.com", model.Subject, $"From: {model.Name} - {model.Email}, Message: {model.Message}");
-        ViewBag.UserMessage = "Mail Sent";
-        ModelState.Clear();
-      }
-
-      return View();
-    }
-
-    public IActionResult About()
-    {
-      ViewBag.Title = "About Us";
-
-      return View();
-    }
-
-    [Authorize]
-    public IActionResult Shop()
-    {
-      var results = _repository.GetAllProducts();
-
-      return View(results);
-    }
-
-  }
 }
