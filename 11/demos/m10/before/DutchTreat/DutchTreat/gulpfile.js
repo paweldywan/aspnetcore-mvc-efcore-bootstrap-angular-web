fd589fce81f1f5fff44ca7b07b03a33c596332e7
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-09-10 11:29:00 +0200

    Development

---
+++ 11/demos/m10/before/DutchTreat/DutchTreat/gulpfile.js
@@ -0,0 +1,13 @@
+﻿/// <binding AfterBuild='default' />
+var gulp = require("gulp");
+var uglify = require("gulp-uglify");
+var concat = require("gulp-concat");
+
+gulp.task("minify", function () {
+    return gulp.src("wwwroot/js/**/*.js") // Wszystkie pliki o rozszerzeniu js z folderu js znajdującym się w wwwroot i jego podfolderów
+        .pipe(uglify()) // Minifikacja plików
+        .pipe(concat("dutchtreat.min.js")) // Złączenie plików w jeden plik
+        .pipe(gulp.dest("wwwroot/dist")); // Zapisanie plików
+});
+
+gulp.task('default', gulp.series('minify'));
