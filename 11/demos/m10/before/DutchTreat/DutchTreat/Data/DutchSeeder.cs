cee971cd21205216239604234fa5cf1e637af18f
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-09-11 01:05:30 +0200

    Fix

--- 11/demos/m10/before/DutchTreat/DutchTreat/Data/DutchSeeder.cs
+++ 11/demos/m10/before/DutchTreat/DutchTreat/Data/DutchSeeder.cs
@@ -11,68 +11,67 @@ using Newtonsoft.Json;
 
 namespace DutchTreat.Data
 {
-  public class DutchSeeder
-  {
-    private readonly DutchContext _ctx;
-    private readonly IWebHostEnvironment _hosting;
-    private readonly UserManager<StoreUser> _userManager;
-
-    public DutchSeeder(DutchContext ctx, IWebHostEnvironment hosting, UserManager<StoreUser> userManager)
+    public class DutchSeeder
     {
-      _ctx = ctx;
-      _hosting = hosting;
-      _userManager = userManager;
-    }
+        private readonly DutchContext _ctx;
+        private readonly IWebHostEnvironment _hosting;
+        private readonly UserManager<StoreUser> _userManager;
 
-    public async Task SeedAsync()
-    {
-      _ctx.Database.EnsureCreated();
-
-      // Seed the Main User
-      StoreUser user = await _userManager.FindByEmailAsync("shawn@dutchtreat.com");
-      if (user == null)
-      {
-        user = new StoreUser()
+        public DutchSeeder(DutchContext ctx, IWebHostEnvironment hosting, UserManager<StoreUser> userManager)
         {
-          LastName = "Wildermuth",
-          FirstName = "Shawn",
-          Email = "shawn@dutchtreat.com",
-          UserName = "shawn@dutchtreat.com"
-        };
-
-        var result = await _userManager.CreateAsync(user, "P@ssw0rd!");
-        if (result != IdentityResult.Success)
-        {
-          throw new InvalidOperationException("Could not create user in Seeding");
+            _ctx = ctx;
+            _hosting = hosting;
+            _userManager = userManager;
         }
-      }
 
-      if (!_ctx.Products.Any())
-      {
-        // Need to create sample data
-        var filepath = Path.Combine(_hosting.ContentRootPath, "Data/art.json");
-        var json = File.ReadAllText(filepath);
-        var products = JsonConvert.DeserializeObject<IEnumerable<Product>>(json);
-        _ctx.Products.AddRange(products);
-
-        var order = _ctx.Orders.Where(o => o.Id == 1).FirstOrDefault();
-        if (order != null)
+        public async Task SeedAsync()
         {
-          order.User = user;
-          order.Items = new List<OrderItem>()
-          {
-            new OrderItem()
+            _ctx.Database.EnsureCreated();
+
+            // Seed the Main User
+            StoreUser user = await _userManager.FindByEmailAsync("shawn@dutchtreat.com");
+            if (user == null)
             {
-              Product = products.First(),
-              Quantity = 5,
-              UnitPrice = products.First().Price
-            }
-          };
-        }
+                user = new StoreUser()
+                {
+                    LastName = "Wildermuth",
+                    FirstName = "Shawn",
+                    Email = "shawn@dutchtreat.com",
+                    UserName = "shawn@dutchtreat.com"
+                };
 
-        _ctx.SaveChanges();
-        
-      }
+                var result = await _userManager.CreateAsync(user, "P@ssw0rd!");
+                if (result != IdentityResult.Success)
+                {
+                    throw new InvalidOperationException("Could not create user in Seeding");
+                }
+            }
+
+            if (!_ctx.Products.Any())
+            {
+                // Need to create sample data
+                var filepath = Path.Combine(_hosting.ContentRootPath, "Data/art.json");
+                var json = File.ReadAllText(filepath);
+                var products = JsonConvert.DeserializeObject<IEnumerable<Product>>(json);
+                _ctx.Products.AddRange(products);
+
+                var order = _ctx.Orders.Where(o => o.Id == 1).FirstOrDefault();
+                if (order != null)
+                {
+                    order.User = user;
+                    order.Items = new List<OrderItem>()
+                    {
+                        new OrderItem()
+                        {
+                          Product = products.First(),
+                          Quantity = 5,
+                          UnitPrice = products.First().Price
+                        }
+                    };
+                }
+
+                _ctx.SaveChanges();
+            }
+        }
     }
-  }
 }
